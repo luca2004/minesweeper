7236635c473a65fdd8d43c0bbe219447ad8699ff
Luca Caflisch <luca.caflisch@gmail.com>
2020-12-27 11:04:33 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-app>
+    <v-main>   
+      <v-row>        
+        <v-col cols="8">
+          <grid />
+          <timer />
+        </v-col>
+        <v-col cols="4"><HelpBlock /></v-col>
+      </v-row>   
+      
+      
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Grid from "./components/Grid";
+import Timer from "./components/Timer";
+import HelpBlock from "./components/HelpBlock";
+//import GitHub from "./components/GitHub";
+
+export default {
+  name: "App",
+
+  components: {
+    Grid,
+    Timer,
+    HelpBlock
+    //GitHub
+  },
+
+  data: () => ({
+    //
+  })
+};
+</script>
+<style>
+#app {
+  background: #bbb;
+}
+</style>
+
+/**
+app background
+card theme
+on hover card
+ */
